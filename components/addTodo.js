169759692fceaf007033fa889c66d671b7ae2edb
mutasem <motasemimad10@gmail.com>
2022-02-27 16:12:57 +0200

    Todo app part (3)

---
+++ components/addTodo.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function AddTodo({ submitHandler }) {
+  const [text, setText] = useState('');
+
+  const changeHandler = (val) => {
+    setText(val);
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder='new todo...'
+        onChangeText={changeHandler}
+      />
+      <Button
+        title='add todo'
+        onPress={() => submitHandler(text)}
+        color='coral'
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+});
