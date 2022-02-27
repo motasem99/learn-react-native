85990effa1ea27b2a02f544cb7b9951e7fbc2272
mutasem <motasemimad10@gmail.com>
2022-02-27 16:57:38 +0200

    flexbox basics

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
+import SandBox from './components/sandbox';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -37,6 +38,7 @@ export default function App() {
   };
 
   return (
+    // <SandBox />
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.container}>
         <Header />
@@ -63,8 +65,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 40,
+    flex: 1,
   },
   list: {
+    flex: 1,
     marginTop: 20,
   },
 });
---
+++ components/sandbox.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SandBox() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.boxOne}>One</Text>
+      <Text style={styles.boxTwo}>Two</Text>
+      <Text style={styles.boxThree}>Three</Text>
+      <Text style={styles.boxFour}>four</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'flex-end',
+    paddingTop: 40,
+    backgroundColor: '#ddd',
+  },
+  boxOne: {
+    flex: 1,
+    backgroundColor: 'violet',
+    padding: 10,
+  },
+  boxTwo: {
+    flex: 2,
+    backgroundColor: 'gold',
+    padding: 20,
+  },
+  boxThree: {
+    flex: 1,
+    backgroundColor: 'coral',
+    padding: 30,
+  },
+  boxFour: {
+    flex: 3,
+    backgroundColor: 'skyblue',
+    padding: 40,
+  },
+});
